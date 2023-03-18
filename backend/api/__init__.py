bde7a8616bf4b4554849c18fe4196a611af1c729
Bessong Junior <juniorbesong8@hotmail.com>
2023-03-18 22:00:15 +0100

    added flask-mail

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, render_template
-from flask_restx import API, Resource, fields
+from flask import Flask
 from flask_cors import CORS
+from flask_mail import Mail
 import json
 
 from .models import db
 
 app = Flask(__name__)
 
+mail = Mail()
+mail.init_app(app)
 db.init_app(app)
 CORS(app)
 
